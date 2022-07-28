fbf78d0d668320c9cfeb2e71af2fe3b0f06d006d
ignacio <ignaciogrillocaimary@gmail.com>
2022-07-28 20:42:36 -0300

    correction of sentence parameter null

--- src/routes/input.routes.js
+++ src/routes/input.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { v4 } = require('uuid');
 
 
 
@@ -10,20 +11,28 @@ router.get('/', async (req, res) => {
   // http://localhost:3001/iecho?text=anana
   const { text } = req.query
 
-  try {
+  if(text && text !== '') {
     
-    let alt = text.toLowerCase();
-      
-    let alt2 = text.replace(/\W/g, '').toLowerCase();
-    // console.log(alt2);
-       
-    alt2 === alt2.split('').reverse().join('')
-    ? res.status(200).json({ 'palindrome': true, text: alt.split('').reverse().join('') })
-    : res.status(200).json({ 'palindrome': false, text: alt.split('').reverse().join('') })
+    try {
+    
+      let alt = text.toLowerCase();
+        
+      let alt2 = text.replace(/\W/g, '').toLowerCase();
+      // console.log(alt2);
+         
+      return alt2 === alt2.split('').reverse().join('')
+      ? res.status(200).json({ 'palindrome': true, text: alt.split('').reverse().join(''), id: v4() })
+      : res.status(200).json({ 'palindrome': false, text: alt.split('').reverse().join(''), id: v4() })
+  
+  
+    } catch(error) {
+      res.status(400).json({error: "no text"});
+    }
 
+  } else {
 
-  } catch(error) {
     res.status(400).json({error: "no text"});
+
   }
 
 });
